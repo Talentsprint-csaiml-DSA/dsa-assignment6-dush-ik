79de48feb829d81a7a3a9c21336ac7aa6c7c39e4
dush-ik <dushik@live.com>
2024-11-18 20:20:50 +0530

    feat: box_packing

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-def longest_box_sequence(boxes):
-    #ToDo 
-    #Write your code here.
-    
+def can_fit(box1, box2):
+  return box1[0] <= box2[0] and box1[1] <= box2[1] and box1[2] <= box2[2]
+
+def box_packing(box_list):
+  sorted_box_list = sorted(box_list, key=lambda box: box[0] * box[1] * box[2])
+  total_boxes = len(box_list)
+  dp = [1 for _ in range(total_boxes)]
+  for i in range(total_boxes):
+    current = sorted_box_list[i]
+    for j in range(i):
+      temp = sorted_box_list[j]
+      if can_fit(temp, current):
+        dp[i] = max(dp[j] + 1, dp[i])
+  return max(dp)
+
+print(box_packing([(6, 1, 3), (5, 4, 2), (7, 5, 4), (6, 2, 5), (5, 5, 3)]))
